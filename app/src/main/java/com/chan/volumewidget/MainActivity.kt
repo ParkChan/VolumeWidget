c523b1713083a1ea22fac31ba7ea5ce89397943e
ParkChan <hydrarkk@naver.com>
2021-05-27 16:12:50 +0900

    [추가] Seekbar 세로 스크롤 기능 추가

--- app/src/main/java/com/chan/volumewidget/MainActivity.kt
+++ app/src/main/java/com/chan/volumewidget/MainActivity.kt
@@ -1,11 +1,44 @@
 package com.chan.volumewidget
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import com.chan.volumewidget.databinding.ActivityMainBinding
+import com.chan.widget.VerticalSeekBar
+import timber.log.Timber
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        initBinding()
+        initListener()
+
+    }
+
+    private fun initBinding() {
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        binding.lifecycleOwner = this
+    }
+
+    private fun initListener() {
+        binding.seekBar.setOnBoxedPointsChangeListener(object : VerticalSeekBar.OnValuesChangeListener{
+            override fun onPointsChanged(boxedPoints: VerticalSeekBar?, points: Int) {
+                Timber.d("setOnBoxedPointsChangeListener onPointsChanged points : $points")
+            }
+
+            override fun onStartTrackingTouch(boxedPoints: VerticalSeekBar?) {
+                Timber.d("setOnBoxedPointsChangeListener onStartTrackingTouch")
+            }
+
+            override fun onStopTrackingTouch(boxedPoints: VerticalSeekBar?) {
+                Timber.d("setOnBoxedPointsChangeListener onStopTrackingTouch")
+            }
+
+        })
     }
 }
