f1a2bf7e45be7d01016fb47d6fbd623c71e2191c
ParkChan <hydrarkk@naver.com>
2021-06-07 18:22:15 +0900

    [수정] 기본 가이드에 따라 수정중

--- README.md
+++ README.md
@@ -3,7 +3,8 @@
 
 ## 참조
 * [Build an App Widget](https://developer.android.com/guide/topics/appwidgets)
-* [도움이 된 자료](https://velog.io/@jshme/how-to-use-appwidgetprovider-in-android)
+* [도움이 된 자료1](https://parkbeommin.github.io/android/android-widget)
+* [도움이 된 자료2](https://velog.io/@jshme/how-to-use-appwidgetprovider-in-android)
 * [Seekbar](https://github.com/alpbak/BoxedVerticalSeekBar)
 
 ## 적용 기술
--- app/src/main/java/com/chan/volumewidget/WidgetProvider.kt
+++ app/src/main/java/com/chan/volumewidget/WidgetProvider.kt
@@ -1,95 +1,51 @@
-package com.chan.volumewidget;
+package com.chan.volumewidget
 
+import android.app.PendingIntent
+import android.appwidget.AppWidgetManager
+import android.appwidget.AppWidgetProvider
+import android.content.Context
+import android.content.Intent
+import android.net.Uri
+import android.widget.RemoteViews
+import timber.log.Timber
 
-import android.app.PendingIntent;
-import android.appwidget.AppWidgetManager;
-import android.appwidget.AppWidgetProvider;
-import android.content.ComponentName;
-import android.content.Context;
-import android.content.Intent;
-import android.net.Uri;
-import android.view.Gravity;
-import android.widget.RemoteViews;
-import android.widget.Toast;
+class WidgetProvider : AppWidgetProvider() {
 
-import timber.log.Timber;
+    private val MY_ACTION = "android.action.MY_ACTION"
 
-public class WidgetProvider extends AppWidgetProvider {
+    override fun onUpdate(
+        context: Context,
+        appWidgetManager: AppWidgetManager,
+        appWidgetIds: IntArray
+    ) {
+        Timber.d("onUpdate >>> ")
 
-    public static final String EXTRA_CLICKED_FILE = "EXTRA_CLICKED_FILE";
-    public static final String ACTION_WIDGET_UPDATE = "android.appwidget.action.APPWIDGET_UPDATE";
-    private static final String LIST_ITEM_CLICKED_ACTION = "LIST_ITEM_CLICKED_ACTION";
-    private static final String REFRESH_WIDGET_ACTION = "REFRESH_WIDGET_ACTION";
-
-    @Override
-    public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
-
-        Timber.d("onUpdate >>> ");
-        for (int appWidgetId : appWidgetIds) {
-            RemoteViews rv = new RemoteViews(context.getPackageName(), R.layout.widget_layout);
-
-            // The empty view is displayed when the collection has no items. It should be a sibling
-            // of the collection view:
-            rv.setEmptyView(R.id.widget_list, R.id.empty_view);
-
-            // Setup intent which points to the WidgetService which will provide the views for this collection.
-            Intent intent = new Intent(context, WidgetService.class);
-            intent.putExtra(AppWidgetManager.EXTRA_APPWIDGET_ID, appWidgetId);
-            // When intents are compared, the extras are ignored, so we need to embed the extras
-            // into the data so that the extras will not be ignored.
-            intent.setData(Uri.parse(intent.toUri(Intent.URI_INTENT_SCHEME)));
-            rv.setRemoteAdapter(R.id.widget_list, intent);
-
-            // Setup refresh button:
-            Intent refreshIntent = new Intent(context, WidgetProvider.class);
-            refreshIntent.setAction(WidgetProvider.REFRESH_WIDGET_ACTION);
-            refreshIntent.putExtra(AppWidgetManager.EXTRA_APPWIDGET_ID, appWidgetId);
-            refreshIntent.setData(Uri.parse(refreshIntent.toUri(Intent.URI_INTENT_SCHEME)));
-            PendingIntent refreshPendingIntent = PendingIntent.getBroadcast(context, 0, refreshIntent, PendingIntent.FLAG_UPDATE_CURRENT);
-            rv.setOnClickPendingIntent(R.id.refresh_button, refreshPendingIntent);
-
-            // Here we setup the a pending intent template. Individuals items of a collection
-            // cannot setup their own pending intents, instead, the collection as a whole can
-            // setup a pending intent template, and the individual items can set a fillInIntent
-            // to create unique before on an item to item basis.
-            Intent toastIntent = new Intent(context, WidgetProvider.class);
-            toastIntent.setAction(WidgetProvider.LIST_ITEM_CLICKED_ACTION);
-            toastIntent.putExtra(AppWidgetManager.EXTRA_APPWIDGET_ID, appWidgetId);
-            intent.setData(Uri.parse(intent.toUri(Intent.URI_INTENT_SCHEME)));
-            PendingIntent toastPendingIntent = PendingIntent.getBroadcast(context, 0, toastIntent, PendingIntent.FLAG_UPDATE_CURRENT);
-            rv.setPendingIntentTemplate(R.id.widget_list, toastPendingIntent);
-
-            appWidgetManager.updateAppWidget(appWidgetId, rv);
+        appWidgetIds.forEach { appwidgetId ->
+            val views: RemoteViews = addViews(context)
+            appWidgetManager?.updateAppWidget(appWidgetIds, views)
         }
     }
 
-
-
-    @Override
-    public void onReceive(Context context, Intent intent) {
-        super.onReceive(context, intent);
-
-        switch (intent.getAction()) {
-            case LIST_ITEM_CLICKED_ACTION:
-                String clickedFilePath = intent.getStringExtra(EXTRA_CLICKED_FILE);
-                Toast toast = Toast.makeText(context, "LIST_ITEM_CLICKED_ACTION: " + clickedFilePath, Toast.LENGTH_SHORT);
-                toast.setGravity(Gravity.CENTER, 0, 0);
-                toast.show();
-                break;
-            case REFRESH_WIDGET_ACTION:
-                int appWidgetId = intent.getIntExtra(AppWidgetManager.EXTRA_APPWIDGET_ID, AppWidgetManager.INVALID_APPWIDGET_ID);
-                AppWidgetManager.getInstance(context).notifyAppWidgetViewDataChanged(appWidgetId, R.id.widget_list);
-
-                Toast toast2 = Toast.makeText(context, "REFRESH_WIDGET_ACTION", Toast.LENGTH_SHORT);
-                toast2.setGravity(Gravity.CENTER, 0, 0);
-                toast2.show();
-                break;
-            case ACTION_WIDGET_UPDATE:
-                AppWidgetManager appWidgetManager = AppWidgetManager.getInstance(context);
-                int appWidgetIds[] = appWidgetManager.getAppWidgetIds(new ComponentName(context, WidgetProvider.class));
-                appWidgetManager.notifyAppWidgetViewDataChanged(appWidgetIds, R.id.widget_list);
-                break;
-        }
+    override fun onReceive(context: Context, intent: Intent) {
+        super.onReceive(context, intent)
     }
+
+    private fun addViews(context: Context?): RemoteViews {
+        val views = RemoteViews(context?.packageName, R.layout.widget_layout)
+        views.setOnClickPendingIntent(R.id.btn_plus, setMyAction(context))
+        views.setOnClickPendingIntent(R.id.btn_minus, buildURIIntent(context))
+        return views
+    }
+
+    private fun setMyAction(context: Context?): PendingIntent {
+        val intent = Intent(MY_ACTION)
+        return PendingIntent.getBroadcast(context, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT)
+    }
+
+    private fun buildURIIntent(context: Context?): PendingIntent {
+        val intent = Intent(Intent.ACTION_VIEW).setData(Uri.parse("http://parkbeommin.github.io"))
+        return PendingIntent.getActivity(context, 0, intent, 0)
+    }
+
 
 }
