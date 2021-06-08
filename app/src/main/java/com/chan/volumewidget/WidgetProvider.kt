c50016a3f13487d54f8aaaf88e96390042897c28
ParkChan <hydrarkk@naver.com>
2021-06-08 18:19:20 +0900

    [수정] 오늘도 밍기적..

--- app/src/main/java/com/chan/volumewidget/WidgetProvider.kt
+++ app/src/main/java/com/chan/volumewidget/WidgetProvider.kt
@@ -3,49 +3,62 @@ package com.chan.volumewidget
 import android.app.PendingIntent
 import android.appwidget.AppWidgetManager
 import android.appwidget.AppWidgetProvider
+import android.content.ComponentName
 import android.content.Context
 import android.content.Intent
 import android.net.Uri
 import android.widget.RemoteViews
 import timber.log.Timber
 
+
 class WidgetProvider : AppWidgetProvider() {
 
     private val MY_ACTION = "android.action.MY_ACTION"
 
     override fun onUpdate(
-        context: Context,
-        appWidgetManager: AppWidgetManager,
-        appWidgetIds: IntArray
+            context: Context,
+            appWidgetManager: AppWidgetManager,
+            appWidgetIds: IntArray
     ) {
         Timber.d("onUpdate >>> ")
+        val thisWidget = ComponentName(context, WidgetProvider::class.java)
+        val allWidgetIds = appWidgetManager.getAppWidgetIds(thisWidget)
 
-        appWidgetIds.forEach { appwidgetId ->
-            val views: RemoteViews = addViews(context)
-            appWidgetManager?.updateAppWidget(appWidgetIds, views)
+        for (widgetId in allWidgetIds) {
+            val views = RemoteViews(context.packageName, R.layout.widget_layout)
+            views.setOnClickPendingIntent(R.id.btn_plus, setMyAction(context))
+            views.setOnClickPendingIntent(R.id.btn_minus, buildURIIntent(context))
+            appWidgetManager.updateAppWidget(appWidgetIds, views)
         }
     }
 
     override fun onReceive(context: Context, intent: Intent) {
         super.onReceive(context, intent)
-    }
-
-    private fun addViews(context: Context?): RemoteViews {
-        val views = RemoteViews(context?.packageName, R.layout.widget_layout)
-        views.setOnClickPendingIntent(R.id.btn_plus, setMyAction(context))
-        views.setOnClickPendingIntent(R.id.btn_minus, buildURIIntent(context))
-        return views
+        Timber.d("onReceive >>> ${intent.data}")
+        val action = intent.action
+//        if (action == MY_ACTION) {
+//            //버튼 클릭 결과를 로그로 확인합니다.
+//            Timber.d("onReceive >>> 이벤트클릭 테스트")
+//            val appWidgetManager = AppWidgetManager.getInstance(context)
+//            val remoteViews = RemoteViews(context.packageName, R.layout.widget_layout)
+//            val componentName = ComponentName(context, WidgetProvider::class.java)
+//            remoteViews.setTextViewText(R.id.btn_plus, "이벤트발생!")
+//            appWidgetManager.updateAppWidget(componentName, remoteViews)
+//        }
     }
 
     private fun setMyAction(context: Context?): PendingIntent {
+        Timber.d("PendingIntent setMyAction >>> ")
         val intent = Intent(MY_ACTION)
         return PendingIntent.getBroadcast(context, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT)
     }
 
     private fun buildURIIntent(context: Context?): PendingIntent {
+        Timber.d("PendingIntent buildURIIntent >>> ")
         val intent = Intent(Intent.ACTION_VIEW).setData(Uri.parse("http://parkbeommin.github.io"))
         return PendingIntent.getActivity(context, 0, intent, 0)
     }
+
 
 
 }
